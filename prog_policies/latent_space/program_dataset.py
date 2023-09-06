1104c239ab422266cd179adfaa850b8f147f913c
Tales Carvalho <tales-h-carvalho@hotmail.com>
2023-09-06 15:38:27 -0600

    Data loader for new dataset format

--- prog_policies/latent_space/program_dataset.py
+++ prog_policies/latent_space/program_dataset.py
@@ -112,6 +112,66 @@ class ProgramPerceptionsDataset(ProgramDataset):
         return perc_h, a_h, a_h_mask, prog, prog_mask, struct
 
 
+class ProgramPerceptionsDataset2(ProgramDataset):
+    
+    def __init__(self, program_list: list, dsl: BaseDSL, device: torch.device, max_program_length=45, max_demo_length=100):
+        super().__init__(program_list, dsl, device, max_program_length, max_demo_length)
+        structure_dsl = dsl.structure_only()
+        self.struct_parse_str_to_int = structure_dsl.parse_str_to_int
+        self.struct_pad_token = structure_dsl.t2i['<pad>']
+        self.struct_hole_token = structure_dsl.t2i['<HOLE>']
+        self.domain_specific = [action for action in dsl.get_actions()] +\
+            [bool_feature for bool_feature in dsl.get_bool_features()] +\
+            [int_feature for int_feature in dsl.get_int_features()] +\
+            [f'R={i}' for i in range(20)]
+        self.domain_tokens = [dsl.t2i[token] for token in self.domain_specific]
+    
+    def __getitem__(self, idx):
+        prog_str, (_, data_perc_h, data_a_h) = self.programs[idx]
+        
+        prog_int = self.parse_str_to_int(prog_str)
+        
+        # replace every domain specific token in prog_int by <HOLE>
+        struct_int = [self.struct_hole_token if token in self.domain_tokens else token for token in prog_int]
+        # for token in prog_int:
+        #     if token in self.domain_tokens:
+        #         struct_int.append(self.struct_hole_token)
+        #     else:
+        #         struct_int.append(token)
+        
+        prog = np.array(prog_int)
+
+        prog = torch.from_numpy(prog).to(self.device).to(torch.long)
+        program_len = prog.shape[0]
+        prog_sufix = torch.tensor((self.max_program_len - program_len - 1) * [self.pad_token],
+                                  device=self.device, dtype=torch.long)
+        prog = torch.cat((prog, prog_sufix))
+        
+        struct = np.array(struct_int)
+        
+        struct = torch.from_numpy(struct).to(self.device).to(torch.long)
+        struct_sufix = torch.tensor((self.max_program_len - program_len - 1) * [self.struct_pad_token],
+                                    device=self.device, dtype=torch.long)
+        struct = torch.cat((struct, struct_sufix))
+        
+        a_h_expanded = np.ones((len(data_a_h), self.max_demo_length), dtype=int) * (self.action_nop)
+        perc_h_expanded = np.zeros((len(data_perc_h), self.max_demo_length, *data_perc_h[0][0].shape), dtype=bool)
+
+        # Add no-op actions for empty demonstrations
+        for i in range(len(data_a_h)):
+            a_h_expanded[i, 1:len(data_a_h[i])+1] = data_a_h[i]
+            perc_h_expanded[i, :len(data_a_h[i])+1] = data_perc_h[i]
+            perc_h_expanded[i, len(data_a_h[i])+1:] = data_perc_h[i][-1] * (self.max_demo_length - len(data_a_h[i]) + 1)
+        
+        perc_h = torch.tensor(perc_h_expanded, device=self.device, dtype=torch.float32)
+        a_h = torch.tensor(a_h_expanded, device=self.device, dtype=torch.long)
+
+        prog_mask = (prog != self.pad_token)
+        a_h_mask = (a_h != self.action_nop)
+
+        return perc_h, a_h, a_h_mask, prog, prog_mask, struct
+
+
 class ProgramOnlyDataset(ProgramDataset):
     
     def __getitem__(self, idx):
