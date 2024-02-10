94cf088c46d5bd8433e3ad13efc690009a2cc3fa
Tales Carvalho <tales-h-carvalho@hotmail.com>
2024-02-10 11:34:36 -0700

    Remove latent_space2

--- prog_policies/search_space/__init__.py
+++ prog_policies/search_space/__init__.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from .base_space import BaseSearchSpace
 from .latent_space import LatentSpace
-from .latent_space2 import LatentSpace2
 from .programmatic_space import ProgrammaticSpace
 
 def get_search_space_cls(search_space_cls_name: str) -> type[BaseSearchSpace]:
